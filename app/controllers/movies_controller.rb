f99ea0983d31aca586c95988d8588ef9656b19bf
saasbook <saasbook@saasbook.(none)>
2013-10-06 22:27:43 -0700

    beta version

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,10 +9,13 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
     @all_ratings = Movie.get_ratings
+    @need_redirect = false
 
     #Retrieve attribute to filter by
     if params['options'] == nil
       @options = session[:sorting_option]
+      params['options'] = session[:sorting_option]
+      @need_redirect = true if session[:sorting_option] != nil
     else
       @options = params['options']
       session[:sorting_option] = params['options']
@@ -22,14 +25,20 @@ class MoviesController < ApplicationController
     if session[:enabled_ratings] == nil
       session[:enabled_ratings] = Hash.new
       @all_ratings.each{|rating| session[:enabled_ratings][rating] = 1}
+      params[:sorting_option] = session[:enabled_ratings]
+      @need_redirect = true
     elsif params[:ratings] != nil
       session[:enabled_ratings] = params[:ratings]
+    elsif params[:ratings] == nil
+      params[:ratings] = session[:enabled_ratings]
+      @need_redirect = true
     end
 
     @movies = Movie.find(:all, :conditions => ["rating IN (?)", session[:enabled_ratings].keys])
 
     @movies = @movies.sort_by {|movie| movie.title} if @options == 'sort_by_title'
     @movies = @movies.sort_by {|movie| movie.release_date} if @options == 'sort_by_date'
+    redirect_to movies_path(params) if @need_redirect
   end
 
   def create
